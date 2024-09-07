3a16eb43e26a1573b1daa27d67f38aa28ff0b2e2
kyung <bamtori1029@gmail.com>
2024-09-07 22:47:24 +0900

    initial commit

---
+++ Hamzzimall_BE/src/main/java/com/hamzzimall/HamzzimallBeApplication.java
@@ -0,0 +1,14 @@
+package com.hamzzimall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class HamzzimallBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HamzzimallBeApplication.class, args);
+	}
+
+}
---
+++ Hamzzimall_BE/src/test/java/com/hamzzimall/HamzzimallBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamzzimall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HamzzimallBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
